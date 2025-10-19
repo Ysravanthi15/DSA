13427dee7efbce32135a93efadef6fda694c67c4
Sravanthi Yedla <167872100+Ysravanthi15@users.noreply.github.com>
2025-10-19 19:30:49 +0530

    Time: 5 ms (96.94%) | Memory: 44.8 MB (15.37%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length-k+1;i++){
+            int j=i+k-1;
+            int temp=nums[j]-nums[i];
+            ans=Math.min(ans,temp);
+        }
+        return ans;
+    }
+}
